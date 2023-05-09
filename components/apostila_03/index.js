7e2b397281482dcf3ab6e7b49422cf3ed11a4d97
Victor Pacheco <victor.pacheco2711@hotmail.com>
2023-05-09 19:49:59 -0300

    aulainiciomobile

---
+++ components/apostila_03/index.js
@@ -0,0 +1,13 @@
+import {  Text, View } from 'react-native';
+
+import styles from './styles';
+
+export default function apostila03() {
+  return (
+    <View style={styles.container}>
+      <Text>O Exemplo apostila 3</Text>
+        </View>
+  );
+}
+
+
